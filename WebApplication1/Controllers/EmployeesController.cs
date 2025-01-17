96800fd88018f9883ed575be6d7273d3799185f5
Mohammed.H.M.Abushabab <abu.wesam.shabab@gmail.com>
2025-01-18 00:58:23 +0300

    Initial commit

---
+++ WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        List<Employee> employeesList =new List<Employee>{
+            new Employee{
+                Id=1,
+                Name="ahmad",
+                Cite="amman",
+                HDate=DateTime.Now,
+                IsActive=true,
+                Salary=1500,
+
+            }
+            };
+        public IActionResult AllEmployees()
+        {
+            return View(employeesList);
+
+        }
+        public IActionResult Details(int? id)
+            
+        {
+            if (id == null) 
+            {
+                return RedirectToAction("AllEmployees");
+
+            }
+            var empData =(from x in employeesList
+                where x.Id==id
+                select x).SingleOrDefault();
+
+            return View(empData); 
+        
+
+        }
+    }
+}
---
+++ WebApplication1/Models/Class.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models
+
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cite{ get; set; }
+        public decimal Salary { get; set; }
+        public DateTime HDate { get; set; }
+        public bool IsActive { get; set; }
+    
+    
+    
+    
+    }
+}
